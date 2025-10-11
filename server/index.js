c56f26d7f8a1f85f2187965855c867cd0239a0af
AidaCodez <211214391+AidaCodez@users.noreply.github.com>
2025-10-11 22:11:55 +0800

    feat(db): get all todo
    Include a get req to get all data from db
    Test the  get req with Postman

--- server/index.js
+++ server/index.js
@@ -25,3 +25,13 @@ app.post("/todos", async(req, res) => {
 app.listen(5000, () => {
     console.log("server has started on port 5000")
 });
+
+//get all todo
+app.get("/todos", async(req, res) => {
+    try{
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
+    } catch (err){
+        console.error(err.message);
+    }
+})
