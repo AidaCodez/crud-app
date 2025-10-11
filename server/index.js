301e912fa0a494eb54034f33c783237ad9730f66
AidaCodez <211214391+AidaCodez@users.noreply.github.com>
2025-10-11 22:46:43 +0800

    feat(db): get a todo
    Include a get req to get a specifiq data from db
    Test the  get req url with Postman

--- server/index.js
+++ server/index.js
@@ -35,3 +35,17 @@ app.get("/todos", async(req, res) => {
         console.error(err.message);
     }
 })
+
+//get a todo
+app.get("/todos/:id", async (req, res) => {
+    try{
+        const {id} =req.params;
+        const todo = await pool.query(
+            "SELECT * FROM todo WHERE todo_id = $1", 
+            [id]
+        );
+        res.json(todo.rows[0]);
+    } catch (err){
+        console.error(err.message);
+    }
+})
