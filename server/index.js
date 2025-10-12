fc5df8d98c177d92d2cdfe9ab1ae8e8f9f5a6c95
AidaCodez <211214391+AidaCodez@users.noreply.github.com>
2025-10-12 13:33:44 +0800

    feat(db): delete a todo
    Include a delete req to remove a data from db
    Test the  delete req with Postman

--- server/index.js
+++ server/index.js
@@ -64,3 +64,16 @@ app.put("/todos/:id", async (req, res) => {
         console.error(err.message);
     }
 })
+
+//delete a todo
+app.delete("/todos/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1",
+        [id]
+        );
+        res.json("Todo was deleted!");
+    } catch {
+        console.log(err.message);
+    }
+})
